b23f055e6902f43066a7ee364a9468a868a29172
akielaries <akiel@akiel.org>
2026-02-11 12:27:49 -0800

    cmake-ify

---
+++ build/CMakeFiles/softcore_fw_example.elf.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/akiel/GMD_workspace/softcore_fw_example/bsp/gowin/startup/startup_GOWIN_M1.S" "/home/akiel/GMD_workspace/softcore_fw_example/build/CMakeFiles/softcore_fw_example.elf.dir/bsp/gowin/startup/startup_GOWIN_M1.S.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/akiel/GMD_workspace/softcore_fw_example/src"
+  "/home/akiel/GMD_workspace/softcore_fw_example/bsp/gowin/core"
+  "/home/akiel/GMD_workspace/softcore_fw_example/bsp/gowin/system"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/inc"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/akiel/GMD_workspace/softcore_fw_example/bsp/gowin/system/system_GOWIN_M1.c" "CMakeFiles/softcore_fw_example.elf.dir/bsp/gowin/system/system_GOWIN_M1.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/bsp/gowin/system/system_GOWIN_M1.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_can.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_can.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_can.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_ddr3.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_ddr3.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_ddr3.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_dualtimer.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_dualtimer.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_dualtimer.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_ethernet.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_ethernet.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_ethernet.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_gpio.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_gpio.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_gpio.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_i2c.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_i2c.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_i2c.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_misc.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_misc.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_misc.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_psram.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_psram.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_psram.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_qspi_flash.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_qspi_flash.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_qspi_flash.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_rtc.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_rtc.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_rtc.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_sdcard.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_sdcard.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_sdcard.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_spi.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_spi.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_spi.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_spi_flash.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_spi_flash.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_spi_flash.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_timer.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_timer.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_timer.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_trng.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_trng.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_trng.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_uart.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_uart.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_uart.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/drivers/src/GOWIN_M1_wdog.c" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_wdog.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_wdog.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/src/GOWIN_M1_it.c" "CMakeFiles/softcore_fw_example.elf.dir/src/GOWIN_M1_it.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/src/GOWIN_M1_it.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/src/delay.c" "CMakeFiles/softcore_fw_example.elf.dir/src/delay.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/src/delay.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/src/gpio.c" "CMakeFiles/softcore_fw_example.elf.dir/src/gpio.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/src/gpio.c.o.d"
+  "/home/akiel/GMD_workspace/softcore_fw_example/src/main.c" "CMakeFiles/softcore_fw_example.elf.dir/src/main.c.o" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/src/main.c.o.d"
+  "" "bin/softcore_fw_example.elf" "gcc" "CMakeFiles/softcore_fw_example.elf.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/softcore_fw_example.elf.dir/link.d
@@ -0,0 +1,106 @@
+bin/softcore_fw_example.elf: \
+  /home/akiel/GMD_workspace/softcore_fw_example/bsp/gowin/startup/linker/GOWIN_M1_flash_burn.ld \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/crti.o \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/crtbegin.o \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/crt0.o \
+  CMakeFiles/softcore_fw_example.elf.dir/bsp/gowin/startup/startup_GOWIN_M1.S.o \
+  CMakeFiles/softcore_fw_example.elf.dir/bsp/gowin/system/system_GOWIN_M1.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_can.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_ddr3.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_dualtimer.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_ethernet.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_gpio.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_i2c.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_misc.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_psram.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_qspi_flash.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_rtc.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_sdcard.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_spi.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_spi_flash.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_timer.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_trng.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_uart.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_wdog.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/src/GOWIN_M1_it.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/src/delay.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/src/gpio.c.o \
+  CMakeFiles/softcore_fw_example.elf.dir/src/main.c.o \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/libgcc.a \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/libc.a \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/crtend.o \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/crtn.o \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/libgcc.a \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/libc.a \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/libm.a \
+  /usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/libnosys.a
+
+/home/akiel/GMD_workspace/softcore_fw_example/bsp/gowin/startup/linker/GOWIN_M1_flash_burn.ld:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/crti.o:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/crtbegin.o:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/crt0.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/bsp/gowin/startup/startup_GOWIN_M1.S.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/bsp/gowin/system/system_GOWIN_M1.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_can.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_ddr3.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_dualtimer.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_ethernet.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_gpio.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_i2c.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_misc.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_psram.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_qspi_flash.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_rtc.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_sdcard.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_spi.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_spi_flash.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_timer.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_trng.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_uart.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/drivers/src/GOWIN_M1_wdog.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/src/GOWIN_M1_it.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/src/delay.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/src/gpio.c.o:
+
+CMakeFiles/softcore_fw_example.elf.dir/src/main.c.o:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/libgcc.a:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/libc.a:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/crtend.o:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/crtn.o:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/thumb/v6-m/nofp/libgcc.a:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/libc.a:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/libm.a:
+
+/usr/lib/gcc/arm-none-eabi/14.2.1/../../../arm-none-eabi/lib/thumb/v6-m/nofp/libnosys.a:
